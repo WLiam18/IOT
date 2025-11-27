7a56c0704f174ee12fe38c53f41bb96a5019ecda
William Giftson S <124970025+WLiam18@users.noreply.github.com>
2025-11-27 08:32:01 +0530

    Create publisher.py

---
+++ Ex1/publisher.py
@@ -0,0 +1,12 @@
+import paho.mqtt.client as mqtt
+
+broker = "broker.hivemq.com"
+topic = "test/topic"
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.connect(broker, 1883)
+
+while True:
+    msg = input("Enter message to send: ")
+    client.publish(topic, msg)
+    print("Message Published")
